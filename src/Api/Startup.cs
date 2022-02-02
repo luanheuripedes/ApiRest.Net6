e6b08ec3c56d05c3ce3c8043623c5ce46977bf24
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2022-02-02 09:44:22 -0300

    Adicionado a classe Startup

---
+++ src/Api/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+        
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            
+            app.UseAuthorization();
+            app.MapControllers();
+        }
+
+    }
+}
